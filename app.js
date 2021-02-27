6b9736d1a27de001e210b73b23be3f7776df86a1
Matheus Baldissara <baldissaramatheus@gmail.com>
2021-02-27 20:27:53 -0300

    wip: Exibe movimentacoes posiveis para peca selecionada

--- app.js
+++ app.js
@@ -1,7 +1,18 @@
+const FILES = new Array(8).fill(undefined).map((value, i) => String.fromCharCode('a'.charCodeAt(0) + i));
+const RANKS = new Array(8).fill(undefined).map((_, index) => index + 1);
+
+// TODO limpar highlightedMoves e selectedPiece no release do mouse
+// TODO fazer toda a movimentacao do pawn
+// TODO implementar movimentacao por vez do jogador
+// TODO implementar movimentacao de ataque
+// TODO implementar pontuacao
+// TODO implementar movimentos das outras pecas
+// TODO implementar jogadas especiais
+
 window.onload = () => {
 	setupInitialPiecesPositions();
 	addEventListenerOnSquares();
-}
+};
 
 const setupInitialPiecesPositions = () => {
 	const pawns = new Array(8)
@@ -13,14 +24,10 @@ const setupInitialPiecesPositions = () => {
 	insertIntoRank(7, pawns, 'black');
 	insertIntoRank(2, pawns, 'white');
 	insertIntoRank(1, borderPieces, 'white');
-}
+};
 
 const insertIntoRank = (rank, pieces, type) => {
-	const initialCharCode = 'a'.charCodeAt(0);
-	const files = new Array(8)
-		.fill(undefined)
-		.map((value, i) => String.fromCharCode(initialCharCode + i));
-	files.forEach((file, i) => {
+	FILES.forEach((file, i) => {
 		const id = `${file}${rank}`;
 		const piece = pieces[i];
 		const pieceDomElement = document.createElement('div');
@@ -29,18 +36,71 @@ const insertIntoRank = (rank, pieces, type) => {
 		pieceDomElement.setAttribute('player', type);
 		document.getElementById(id).appendChild(pieceDomElement);
 	});
-}
+};
+
 
 const addEventListenerOnSquares = () => {
 	const squares = Array.from(document.getElementsByClassName('square'));
 	squares.forEach(square => square.addEventListener('click', event => handleClickOnSquare(event)));
-}
+};
 
+/** 
+ * @param { Event } event
+ */
 const handleClickOnSquare = event => {
-	const squareId = event.target.id;
-	const domElement = document.getElementById(squareId);
+	removeSelectedPiece();
+	cleanHighlightedSquares();
+	const coordinates = event.target.id;
+	const piece = getPieceFromCoordinates(coordinates);
+	if (!piece) {
+		console.log(`Peça na coordenada ${coordinates.toUpperCase()}: vazio`);
+		return;
+	}
+	event.target.firstChild.setAttribute('selected', true);
+	const player = event.target.firstChild.attributes.player.value;
+	console.log(`Peça na coordenada ${coordinates.toUpperCase()}: ${piece} ${player}`);
+	const hightlightAvailableMoves = getHighlightAvailableMovesFnBySelectedPiece(piece);
+	hightlightAvailableMoves(player, coordinates);
+};
+
+const removeSelectedPiece = () => {
+	const selectedPiece = document.querySelector('[selected=true]');
+	if (selectedPiece) {
+		selectedPiece.setAttribute('selected', false);
+	}
+};
+
+const cleanHighlightedSquares = () => {
+	 document
+	 	.querySelectorAll('[highlighted]')
+		.forEach(el => el.setAttribute('highlighted', false));
+};
+
+const getPieceFromCoordinates = (coordinates) => {
+	const domElement = document.getElementById(coordinates);
 	const childElement = domElement.firstChild;
 	const piece = childElement && childElement.attributes.piece.value;
-	const player = piece && childElement.attributes.player.value;
-	console.log(`Peça na coordenada ${squareId}: ${piece} ${player}`);
+	return piece;
 }
+
+const getHighlightAvailableMovesFnBySelectedPiece = (piece) => {
+	const MapPiescesToAvailableMovesFn = {
+		pawn: highlightPawnAvailableMoves,
+	};
+	return MapPiescesToAvailableMovesFn[piece] || (() => console.log('A peça selecionada não possui uma função de movimento implementada'));
+};
+
+const highlightPawnAvailableMoves = (player, coordinates) => {
+	const [ file, startRank ] = coordinates.split('');
+	RANKS
+		.filter(rank => rank >= startRank)
+		.filter(rank => getPieceFromCoordinates(`${file}${rank}`) === null)
+		.forEach(rank => highlightSquare(`${file}${rank}`));
+};
+
+const highlightSquare = (coordinate) => {
+	const element = document.getElementById(coordinate);
+	const elementToInsert = element.cloneNode(true);
+	elementToInsert.setAttribute('highlighted', true);
+	element.parentNode.replaceChild(elementToInsert, element);
+}; 
